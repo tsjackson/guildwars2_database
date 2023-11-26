c3fa2a01a5afa24fc702cf7c9ae170075b91bff7
Tyler Jackson <tylerj3@gmail.com>
2023-11-26 13:59:54 -0500

    added the remaining leather crafting levels merged all dataframes into one

--- leather_crafting.py
+++ leather_crafting.py
@@ -6,6 +6,7 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 url = "https://wiki.guildwars2.com/wiki/Leatherworker/Novice_recipes"
+url1 = "https://wiki.guildwars2.com/wiki/Leatherworker/Initiate_recipes"
 url2 = "https://wiki.guildwars2.com/wiki/Leatherworker/Apprentice_recipes"
 url3 = "https://wiki.guildwars2.com/wiki/Leatherworker/Journeyman_recipes"
 url4 = "https://wiki.guildwars2.com/wiki/Leatherworker/Adept_recipes"
@@ -17,29 +18,54 @@ insignia_novice = pd.read_html(url, header=0)[2] # adding a dataframe for the in
 rune_novice = pd.read_html(url, header=0)[5] # adding another dataframe for the rune recipes
 refinement_novice = pd.read_html(url, header=0)[0] # adding another dataframe for the refinement recipes
 
+novice_dataframe = pd.concat([insignia_novice, rune_novice, refinement_novice], ignore_index=True) # merging all Novice dataframes into one
+
+# Initiate recipes
+insignia_initiate = pd.read_html(url1, header=0)[2] # adding a dataframe for the insignia recipes
+rune_initiate = pd.read_html(url1, header=0)[4] # adding another dataframe for the rune recipes
+refinement_initiate = pd.read_html(url1, header=0)[0] # adding another dataframe for the refinement recipes
+
+initiate_dataframe = pd.concat([insignia_initiate, rune_initiate, refinement_initiate], ignore_index=True) # merging all Initiate dataframes into one
+
 # Apprentice recipes
 insignia_apprentice = pd.read_html(url2, header=0)[2] # adding a dataframe for the insignia recipes
 rune_apprentice = pd.read_html(url2, header=0)[4] # adding another dataframe for the rune recipes
 refinement_apprentice = pd.read_html(url2, header=0)[0] # adding another dataframe for the refinement recipes
 
-# # Journeyman recipes
-# insignia_journeyman = pd.read_html(url3, header=0)[2] # adding a dataframe for the insignia recipes
-# rune_journeyman = pd.read_html(url3, header=0)[5] # adding another dataframe for the rune recipes
-# refinement_journeyman = pd.read_html(url3, header=0)[0] # adding another dataframe for the refinement recipes
+apprentice_dataframe = pd.concat([insignia_apprentice, rune_apprentice, refinement_apprentice], ignore_index=True) # merging all Apprentice dataframes into one
 
-print(insignia_apprentice)
-print(rune_apprentice)
-print(refinement_apprentice)
+# Journeyman recipes
+insignia_journeyman = pd.read_html(url3, header=0)[2] # adding a dataframe for the insignia recipes
+rune_journeyman = pd.read_html(url3, header=0)[5] # adding another dataframe for the rune recipes
+refinement_journeyman = pd.read_html(url3, header=0)[0] # adding another dataframe for the refinement recipes
 
-# merging the dataframes together
-leather_working = pd.concat([insignia_novice, rune_novice, refinement_novice, insignia_apprentice, rune_apprentice, refinement_apprentice], ignore_index=True)
+journeyman_dataframe = pd.concat([insignia_journeyman, rune_journeyman, refinement_journeyman], ignore_index=True) # merging all journeyman dataframes into one
 
-# changing Rating column name
-leather_working.rename(columns={'Rating': 'Level'}, inplace=True)
+# Adept recipes
+insignia_adept = pd.read_html(url4, header=0)[2] # adding a dataframe for the insignia recipes
+rune_adept = pd.read_html(url4, header=0)[4] # adding another dataframe for the rune recipes
+refinement_adept = pd.read_html(url4, header=0)[0] # adding another dataframe for the refinement recipes
 
-# dropping disciplines column
-leather_working.drop(['Discipline(s)'], axis=1, inplace=True)
+adept_dataframe = pd.concat([insignia_adept, rune_adept, refinement_adept], ignore_index=True) # merging all adept dataframes into one
 
-# with sqlite3.connect('gw2.db') as conn:
-#     leather_working.to_sql('leather_crafting', conn, if_exists='replace', index=False)
-#     conn.commit()
+# Master recipes
+insignia_master = pd.read_html(url5, header=0)[3] # adding a dataframe for the insignia recipes
+rune_master = pd.read_html(url5, header=0)[5] # adding another dataframe for the rune recipes
+refinement_master = pd.read_html(url5, header=0)[0] # adding another dataframe for the refinement recipes
+
+master_dataframe = pd.concat([insignia_master, rune_master, refinement_master], ignore_index=True) # merging all master dataframes into one
+
+# Grandmaster recipes
+insignia_grandmaster = pd.read_html(url6, header=0)[3] # adding a dataframe for the insignia recipes
+# Grandmaster does not have runes
+# Grandmaster does not have refinement
+
+leather_working = pd.concat([novice_dataframe, initiate_dataframe, apprentice_dataframe, journeyman_dataframe, adept_dataframe, master_dataframe, insignia_grandmaster], ignore_index=True) # merging all leatherworking dataframes into one
+
+# data cleaning
+leather_working.rename(columns={'Rating': 'Level'}, inplace=True) # changing Rating column name
+leather_working.drop(['Discipline(s)'], axis=1, inplace=True) # dropping disciplines column
+
+with sqlite3.connect('gw2.db') as conn:
+    leather_working.to_sql('leather_crafting', conn, if_exists='replace', index=False)
+    conn.commit()
