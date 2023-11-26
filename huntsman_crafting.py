9631f475d4c96535ad2226225c3e4d271b5d00c9
Tyler Jackson <tylerj3@gmail.com>
2023-11-26 15:45:59 -0500

    added and tested Initiate and Apprentice Huntsman crafting

--- huntsman_crafting.py
+++ huntsman_crafting.py
@@ -18,7 +18,28 @@ inscription_novice : pd.DataFrame = pd.read_html(url, header=0)[2] # adding a da
 sigil_novice : pd.DataFrame = pd.read_html(url, header=0)[5] # adding a dataframe for the Sigil recipes
 nourishment_novice : pd.DataFrame = pd.read_html(url, header=0)[6] # adding a dataframe for the Nourishment recipes
 
-novice_dataframe = pd.concat([refinement_novice, inscription_novice, sigil_novice, nourishment_novice], ignore_index=True) # merging all Novice dataframes into one
+novice_dataframe : pd.DataFrame = pd.concat([refinement_novice, inscription_novice, sigil_novice, nourishment_novice], ignore_index=True) # merging all Novice dataframes into one
+
+# Initiate recipes
+refinement_initiate = pd.read_html(url1, header=0)[0] # adding a dataframe for the refinement recipes
+inscription_initiate : pd.DataFrame = pd.read_html(url1, header=0)[2] # adding a dataframe for the Inscription recipes
+sigil_initiate : pd.DataFrame = pd.read_html(url1, header=0)[4] # adding a dataframe for the Sigil recipes
+nourishment_initiate : pd.DataFrame = pd.read_html(url1, header=0)[5] # adding a dataframe for the Nourishment recipes
+
+initiate_dataframe : pd.DataFrame = pd.concat([refinement_initiate, inscription_initiate, sigil_initiate, nourishment_initiate], ignore_index=True) # merging all Initiate dataframes into one
+
+# Apprentice recipes
+refinement_apprentice = pd.read_html(url2, header=0)[0] # adding a dataframe for the refinement recipes
+inscription_apprentice : pd.DataFrame = pd.read_html(url2, header=0)[2] # adding a dataframe for the Inscription recipes
+sigil_apprentice : pd.DataFrame = pd.read_html(url2, header=0)[5] # adding a dataframe for the Sigil recipes
+nourishment_apprentice : pd.DataFrame = pd.read_html(url2, header=0)[6] # adding a dataframe for the Nourishment recipes
+
+apprentice_dataframe : pd.DataFrame = pd.concat([refinement_apprentice, inscription_apprentice, sigil_apprentice, nourishment_apprentice], ignore_index=True) # merging all Apprentice dataframes into one
+
+# print(refinement_apprentice)
+# print(inscription_apprentice)
+# print(sigil_apprentice)
+# print(nourishment_apprentice)
 
 # data cleaning
 novice_dataframe['Item'] = novice_dataframe['Item'].fillna(novice_dataframe['Name']) # replacing null values in the item column with the value in the name column
@@ -29,12 +50,6 @@ novice_dataframe['Attributes'] = novice_dataframe['Attributes'].fillna('N.A.') #
 novice_dataframe.drop(columns=['Discipline(s)'], inplace=True) # dropping the Output column
 novice_dataframe['Crafting Level'] = novice_dataframe['Crafting Level'].fillna(novice_dataframe['Rating']) # replacing null values in the Crafting Level column with the value in the name Rating
 novice_dataframe.drop(columns=['Rating'], inplace=True) # dropping the Rating column
-
-
-# print(refinement_novice)
-# print(inscription_novice)
-# print(sigil_novice)
-# print(nourishment_novice)
 
 with sqlite3.connect('gw2.db') as conn:
     novice_dataframe.to_sql('huntsman_crafting', conn, if_exists='replace', index=False)
