7924127958be31ccc200ed91a707bd3e0b829786
Tyler Jackson <tylerj3@gmail.com>
2023-11-26 11:11:34 -0500

    Added database to .gitignore Added database funcinallity to web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,6 @@
-# creating a web scraper to scrape the data from the wikipedia page and then store it in a csv file
+# creating a web scraper to scrape the data from the wikipedia page and then store it in a database
 
+import sqlite3
 import pandas as pd
 import numpy as np
 from pprint import pprint
@@ -10,10 +11,22 @@ url = "https://wiki.guildwars2.com/wiki/Rune"
 
 df = pd.read_html(url, header=0)[0]
 
-# delete first row
+# creating a column for the type of rune
+df['Quality'] = np.where(df['Rune'].str.contains('Superior'), 'Superior', 'Major')
 
-df = df.iloc[1:]
+# creating columns for the bonuses
+df['Type'] = df['Bonus 1'].str.split(' ', expand=True)[1]
 
-df.to_csv("runes.csv", index=False)
+df['Type'] = np.where(df['Type'].str.contains('to'), 'All', df['Type'])
 
-pprint(df)
+# if quality is major, delete record
+df.drop(df[df['Quality'] == 'Major'].index, inplace=True)
+
+# Moving Quality and type columns to the front
+cols = df.columns.tolist()
+cols = cols[-2:] + cols[:-2]
+df = df[cols]
+
+with sqlite3.connect('gw2.db') as conn:
+    df.to_sql('runes', conn, if_exists='replace', index=False)
+    conn.commit()
