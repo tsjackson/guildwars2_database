a7ea8f887d090c33e40441146fce8d877fd14a52
Tyler Jackson <tylerj3@gmail.com>
2023-11-26 11:39:17 -0500

    data cleanup and added holiday dungeons

--- dungeons.py
+++ dungeons.py
@@ -1,7 +1,6 @@
 import sqlite3
 import pandas as pd
 import numpy as np
-from pprint import pprint
 
 # getting the data from the wikipedia page
 
@@ -12,8 +11,19 @@ df = pd.read_html(url, header=0)[2]
 # dropping the waypoint column
 df.drop(['Waypoint'], axis=1, inplace=True)
 
-pprint(df)
+# replacing the NULL values with N/A
+df.fillna('N/A', inplace=True)
+
+# adding another table for the holiday dungeons
+
+df2 = pd.read_html(url, header=0)[3]
+
+print(df2)
 
 with sqlite3.connect('gw2.db') as conn:
-    df.to_sql('dungeons', conn, if_exists='replace', index=False)
-    conn.commit()
+     df.to_sql('dungeons', conn, if_exists='replace', index=False)
+     conn.commit()
+
+with sqlite3.connect('gw2.db') as conn:
+     df2.to_sql('holiday_dungeons', conn, if_exists='replace', index=False)
+     conn.commit()
