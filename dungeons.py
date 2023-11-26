75098ae7382289eff6e4787148db779e14a891d4
Tyler Jackson <tylerj3@gmail.com>
2023-11-26 12:00:46 -0500

    data cleanup and merged the tables

--- dungeons.py
+++ dungeons.py
@@ -11,19 +11,24 @@ df = pd.read_html(url, header=0)[2]
 # dropping the waypoint column
 df.drop(['Waypoint'], axis=1, inplace=True)
 
-# replacing the NULL values with N/A
-df.fillna('N/A', inplace=True)
-
 # adding another table for the holiday dungeons
 
 df2 = pd.read_html(url, header=0)[3]
 
-print(df2)
+# merging the two tables together
+df = pd.concat([df, df2], axis=0)
+
+# dropping the Suitable utility items column
+df.drop(['Suitable utility items'], axis=1, inplace=True)
+
+# data cleanup
+df['Story Level'].fillna(80, inplace=True)
+df['Explorable Level'].fillna('None', inplace=True)
+df['Foe types [1]'].fillna('Multiple', inplace=True)
+df['Day or Night 2'].fillna('N/A', inplace=True)
+df['Initial Event / Duration'].fillna('N/A', inplace=True)
+df['Availability'].fillna('Always', inplace=True)
 
 with sqlite3.connect('gw2.db') as conn:
      df.to_sql('dungeons', conn, if_exists='replace', index=False)
-     conn.commit()
-
-with sqlite3.connect('gw2.db') as conn:
-     df2.to_sql('holiday_dungeons', conn, if_exists='replace', index=False)
      conn.commit()
